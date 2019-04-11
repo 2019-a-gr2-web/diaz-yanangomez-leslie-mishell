b300b64c1e33bdc0a74ed3e725b1b83b75d353da
lesliediaz <lmdiaz36@gmail.com>
2019-04-11 00:46:23 -0500

    decoradores y post en typescript

--- 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
+++ 01-http/02-servidor-web-nodejs/api-web/src/app.controller.ts
@@ -1,12 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import {Controller, Get, HttpStatus, Post} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get() // Metodo HTTP
+
   getHello(): string {
     return this.appService.getHello();
   }
+  @Post() // Metodo HTTP
+  // @HttpStatus(200) // Codido HTTP a enviar
+  postHello() {
+    return 'Hola mundo en post';
+  }
 }
+
+/*
+@NombreDecoradorClase() //Decorador --> Funcion
+class usuario {
+  @decoradorAtributo
+  atributoPublico; // Si no se le asigna privado o protegido por defecto el atributo es publico
+  private atributoPrivado;
+  protected atributoProtegido;
+  constructor(@Parametro atributoPublic, @OtroParametro atributoPrivado, @OtroOtroParametro atributoProtected ){
+    this.atributoPublico = atributoPublic;
+    this.atributoPrivado = atributoPrivado;
+    this.atributoProtegido= atributoProtected;
+  }
+  @MetodoA()
+  public metodoPublico(){}
+  @MetodoB()
+  private metodoPrivado(){}
+  @MetodoC()
+  protected metodoProtegido(){}
+}*/
