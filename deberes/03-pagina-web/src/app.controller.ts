f9c32a0a2f6303b702eae0b9fe8c0d87344c76ab
lesliediaz <lmdiaz36@gmail.com>
2019-05-21 03:30:54 -0500

    paginawebv1

---
+++ deberes/03-pagina-web/src/app.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Response} from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  @Get('/inicio') // EndPoint
+  getInicio(@Response() res) {
+    return res.render('inicio.ejs');
+  }
+}
